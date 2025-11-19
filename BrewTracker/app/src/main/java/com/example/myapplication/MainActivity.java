dfe4b2d1294418fa4c537df9788e677f7158c66b
Lachezar_Donchev <stu2301321081@uni-plovdiv.bg>
2025-11-19 18:49:25 +0200

    Implemented creating new Brews and saving them in the database.

--- BrewTracker/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BrewTracker/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private BrewViewModel brewViewModel;
+    public static final int NEW_BREW_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +23,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
         final BrewListAdapter adapter = new BrewListAdapter(recyclerView);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         brewViewModel = new ViewModelProvider(this).get(BrewViewModel.class);
 
-        brewViewModel.getAllBrews().observe(this, brews -> {
-            adapter.setBrews(brews);
-        });
+        brewViewModel.getAllBrews().observe(this, adapter::setBrews);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
-            Brew testBrew = new Brew("Test Brew " + System.currentTimeMillis(), "Notes", 4.0f);
-            brewViewModel.insert(testBrew);
+            Intent intent = new Intent(MainActivity.this, NewBrewActivity.class);
+
+            startActivityForResult(intent, NEW_BREW_ACTIVITY_REQUEST_CODE);
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_BREW_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            String name = data.getStringExtra(NewBrewActivity.EXTRA_REPLY_NAME);
+            String notes = data.getStringExtra(NewBrewActivity.EXTRA_REPLY_NOTES);
+            float rating = data.getFloatExtra(NewBrewActivity.EXTRA_REPLY_RATING, 0.0f);
+
+            Brew brew = new Brew(name, notes, rating);
+            brewViewModel.insert(brew);
+
+            Toast.makeText(getApplicationContext(), "Напитката е добавена!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Добавянето е отменено.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }

3bb9dffc541e75c163a5b6a9e3928ccd7f640b7f
Lachezar_Donchev <stu2301321081@uni-plovdiv.bg>
2025-11-19 18:27:57 +0200

    Added a button to the main activity and modified the main screen to show the list of data.

--- BrewTracker/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BrewTracker/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BrewViewModel brewViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final BrewListAdapter adapter = new BrewListAdapter(recyclerView);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        brewViewModel = new ViewModelProvider(this).get(BrewViewModel.class);
+
+        brewViewModel.getAllBrews().observe(this, brews -> {
+            adapter.setBrews(brews);
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Brew testBrew = new Brew("Test Brew " + System.currentTimeMillis(), "Notes", 4.0f);
+            brewViewModel.insert(testBrew);
         });
     }
 }
